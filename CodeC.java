beb14a98e47f0086f4479399a7e1b1888fb56ca9
Ravi Shankar <58580532+ravyshankar@users.noreply.github.com>
2024-09-08 21:19:29 +0800

    edited file1, added code*.java, created fork-branch

---
+++ CodeC.java
@@ -0,0 +1,6 @@
+1. I fork a public repo and name it xyz
+2. Then I clone it to my laptop.
+3. Work on the code changes on my local laptop. 
+4. Then I sync fork. 
+5. I git pull from the forked repo to get the changed files. 
+6. Then I git push -setup-upstream-to origin fork-branch
